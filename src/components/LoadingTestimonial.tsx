e3c815ffe070805e716efe6628606f18b62d33ce
arthurMancioSales <arthur_mancio@outlook.com>
2023-07-08 11:10:26 -0300

    feat: change most placeholder pictures by actual content

--- src/components/LoadingTestimonial.tsx
+++ src/components/LoadingTestimonial.tsx
@@ -1,23 +1,28 @@
+import Container from "./ui/Container";
+
 export default function LoadingTestimonial() {
     return (
-        <>
-            <div className="border border-blue-900 bg-blue-950 shadow rounded-md p-4 max-w-sm w-full mx-auto h-[425px]">
-                <div className="animate-pulse flex flex-wrap">
-                    <div className="justify-center w-full flex">
-                        <div className="rounded-full self-center bg-slate-700 h-10 w-10"></div>
+        <Container
+            direction="flex-col"
+            width="w-screen"
+        >
+            <div className="border border-blue-900 bg-blue-950 shadow rounded-md p-4 max-w-sm w-full mx-auto h-[425px] lg:min-h-[425px] lg:max-w-3xl">
+                <div className="flex flex-wrap animate-pulse">
+                    <div className="flex justify-center w-full">
+                        <div className="self-center w-10 h-10 rounded-full bg-slate-700"></div>
                     </div>
-                    <div className="flex-1 space-y-6 py-5">
-                        <div className="h-2 bg-slate-700 rounded"></div>
+                    <div className="flex-1 py-5 space-y-6">
+                        <div className="h-2 rounded bg-slate-700"></div>
                         <div className="space-y-3">
-                            <div className="h-2 bg-slate-700 rounded"></div>
+                            <div className="h-2 rounded bg-slate-700"></div>
                         </div>
                         <div className="grid grid-cols-3 gap-4">
-                            <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                            <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+                            <div className="h-2 col-span-2 rounded bg-slate-700"></div>
+                            <div className="h-2 col-span-1 rounded bg-slate-700"></div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </Container>
     );
 }
